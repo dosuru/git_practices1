80352f1f850906a52517fda8baeb7bf383fee42f
TULASIRAO <dosuru.tulasirao@capgemini.com>
2023-08-17 23:46:31 +0530

    changes made by master

--- Student.java
+++ Student.java
@@ -2,5 +2,6 @@ public class Student{
     public static void main(Strin[] args){
         System.out.println("java")
         System.out.println("java 8 feature")
+        System.out.println("python")
     }
 }
